359125f6e494af9507b1bb8ceff7d594eab50240
Ale Vasquez <jose5m45@gmail.com>
2022-05-09 16:50:56 -0600

    first commit

---
+++ src/test/java/com/crobatair/learning/saga/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crobatair.learning.saga.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
